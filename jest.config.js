ccef557e432d56e959af8cf5522490ce79e732b2
Larychev93 <larychev93@gmail.com>
2021-08-28 17:20:30 +0300

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,9 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageReporters: [
+    "json-summary",
+    "text",
+    "lcov"
+  ]
 }
